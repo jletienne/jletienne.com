accbd0af8312fe9b29e87d3de14afad369d3c5c0
Jean-Luc Etienne <joe1@williams.edu>
2016-05-25 22:57:17 -0500

    level 2

--- webs.py
+++ webs.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template,request, redirect, url_for
+
 app = Flask(__name__)
+
+@app.route('/', methods = ['POST','GET'])
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
