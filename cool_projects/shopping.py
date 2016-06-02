09b7dbb105e05f70ec950754cdc19538646c4903
Jean-Luc Etienne <joe1@williams.edu>
2016-06-01 21:25:02 -0500

    changes

--- cool_projects/shopping.py
+++ cool_projects/shopping.py
@@ -4,7 +4,7 @@ import random
 import json
 import re
 
-def asics():
+def asics(max_price=100):
     URL = 'http://www.onitsukatiger.com/us/en-us/mens/c/mens-shoes?q=:relevance:gender:MALE:shoeSizeCode:10.5:model:Colorado%20Eighty-Five'
     def html_source(URL):
         return requests.get(URL).text
@@ -30,18 +30,17 @@ def asics():
     costs = [float(i) for i in re.findall(make_regex2(), html_source(URL))]
     products = list(zip(costs,shoes))
 
-    quant = len(get_sales(products=products, max_price = 100))
+    quant = len(get_sales(products=products, max_price=max_price))
 
     if quant >0:
         return('hey john luck there are {} shoes that would interest you'.format(quant))
     else:
         return(rejection())
 
-def jcrew():
+def jcrew(max_price=50):
     URL = 'https://www.jcrew.com/search2/index.jsp?N=21+16+10002+11998&Nloc=en&Ntrm=&Npge=1&Nrpp=48&Nsrt=3&hasSplitResults=false'
 
     def html_source(URL):
-        #print(requests.get(URL).text)
         return requests.get(URL).text
 
     def make_regex():
@@ -57,6 +56,7 @@ def jcrew():
     def make_regex3():
         ips = ['i>In stores & online:</i> (.*?)%', ' off']
         prices = "".join(ips)
+        print(ips)
         return prices
 
     def get_sales(products, max_price):
@@ -69,20 +69,21 @@ def jcrew():
     def get_discount():
         try:
             discount = re.findall(make_regex2(), html_source(URL))[0]
-            return discount
+            return re.sub(r'[^0-9]', '', str(discount)) #numbers only
         except:
             try:
                 discount = re.findall(make_regex3(), html_source(URL))[0]
-                return discount
+                return re.sub(r'[^0-9]', '', str(discount)) #numbers only
             except:
                 return 0
 
-    discount=get_discount()
+    discount= int(str(get_discount())[0:2])
+
     print('the discount is {}%'.format(discount)) #error handling
 
     products = [float(i)*(1-float(discount)*.01) for i in re.findall(make_regex(), html_source(URL))]
 
-    quant = len(get_sales(products=products, max_price = 50))
+    quant = len(get_sales(products=products, max_price = max_price))
 
     if quant >0:
         return('yoooo juan lucas there are {} shirts that may fit you, since the discount is {}%'.format(quant, discount))
