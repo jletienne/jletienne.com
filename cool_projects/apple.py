898e7337b04a603029eaf3c6d64316f5ec280a0a
Jean-Luc Etienne <joe1@williams.edu>
2016-06-03 07:40:34 -0500

    added a space

--- cool_projects/apple.py
+++ cool_projects/apple.py
@@ -22,7 +22,7 @@ def applebot():
    apple = re.findall(make_regex(), html_source(URL))
    apple2 = re.findall(make_regex2(), "".join(apple))[:-1]
    x = "|".join(apple2)
-   x = x.replace('&nbsp;', '')
+   x = x.replace('&nbsp;', ' ')
    x = x.replace('&#8217;', "\\'")
 
    return x
