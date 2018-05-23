8d1c56798d3d6e312be0e3c5e218b9ed5e707392
Jean-Luc Etienne <joe1@williams.edu>
2018-05-22 21:56:32 -0500

    added viewport

--- test.py
+++ test.py
@@ -5,108 +5,73 @@ import re
 import datetime
 import calendar
 
-def getPlayers(team='None'):
-    URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Players.json'.format(team)
 
-    r = requests.get(URL).json()
-    return r
+def addEvent(start_time = '10 pm', location = 'location', opponent = 'Opponent',year=3000, month=1, day=1, team='None'):
+    event_date = datetime.date(year, month, day)
+    gameday = str(event_date)
+    weekday = calendar.day_name[event_date.weekday()]
 
-def chomba(start_time = 'None', location='None', team='None', opponent = 'None', year=2016, month=1, day=1):
-    my_date = datetime.date(year, month, day)
-    gameday = str(my_date)
-    weekday = calendar.day_name[my_date.weekday()]
     URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Events/{}.json'.format(team, gameday)
+    players = getPlayers(team)
 
     gameinfo = {
                     'Start_Time': start_time,
                     'Location': location,
                     'Opponent': opponent,
-                    #'Players': getPlayers(team),
-                    'Date': '{:%b %d, %Y}'.format(my_date),
-                    'Weekday': weekday
+                    'Players': players,
+                    'Date': '{:%b %d, %Y}'.format(event_date),
+                    'Date2': gameday,
+                    'Weekday': weekday,
                 }
 
     r = requests.put(URL, data=json.dumps(gameinfo))
-    print(URL)
-    return r
-
-def getDate(date='Jan 1, 2099'):
-        return datetime.datetime.strptime(date, '%b %d, %Y').date()
-
-def getNextEvent(team='Chupacabras'):
-    URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Events.json'.format(team)
-    r = requests.get(URL).json()
-
-    future_events = {i for i in r if datetime.date.today <= getDate(r[i]['Date'])}
-    next_event = sorted(future_events)[0]
-
-    return next_event
-
-
-def doAll():
-    '''
-    print(chomba(team='Hit_This', start_time='8:50 pm', opponent='Roll Toad', month=7, day=11, year=2016, location='Glencoe Park'))
-    print(chomba(team='Hit_This', start_time='7:30 pm', opponent='The Crystal Mets', month=7, day=18, year=2016, location='Glencoe Park'))
-    print(chomba(team='Hit_This', start_time='9:10 pm', opponent='Balls Deep', month=7, day=25, year=2016, location='Glencoe Park'))
-    print(chomba(team='Hit_This', start_time='8:40 pm & 9:30 pm', opponent='CDC Ballers & Les Miseraballs', month=8, day=1, year=2016, location='Churchill Park'))
-    print(chomba(team='Hit_This', start_time='6:40 pm', opponent='Triage', month=8, day=8, year=2016, location='Glencoe Park'))
-'''
-
-
-def getTeams():
-    URL = 'https://chup-chombas.firebaseio.com/Teams.json'
-    r = requests.get(URL).text
-    return r
-
-
-def incrementTrip(team='None'):
-    URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Visits.json'.format(team)
-    r = requests.get(URL).json()
-    increment = r + 1
-    doIt = requests.put(URL, data=json.dumps(increment))
     return URL
 
-def getEvents(team='None'):
-    URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Events.json'.format(team)
+def getPlayers(team):
+    URL = 'https://chup-chombas.firebaseio.com/Teams/{}/Players.json'.format(team)
     r = requests.get(URL).json()
+
     return r
 
-def getNextPlayerEvent(events='None'):
+def make_regex(x):
+    ips = x
+    products = "".join(ips)
+    return products
 
-    try:
-        future_events = {i for i in events if datetime.date.today() <= getDate(events[i]['Date'])}
-        next_event = sorted(future_events)[0]
-    except:
-        next_event = 'No Next Event'
+def html_source(URL):
+    return requests.get(URL).text
 
-    return next_event
+def add_allen_team(my_team='Red', location='Allen Hockey Rink', URL=None):
 
-def aggregate(player='None'):
-    URL = 'https://chup-chombas.firebaseio.com/Players/{}/Teams.json'.format(player)
-    r = requests.get(URL).json()
+    game_times = re.findall(make_regex(['<td>(.*)</td>']), html_source(URL))
+    game_dates = re.findall(make_regex(['<td class="text-center">(.*?)</td>']), html_source(URL))
 
-    x = {}
+    regex2 = r'<td class=\\"text-left\\"><a href(.*)</a>'
+    home_team1 = re.findall(make_regex([regex2]), html_source(URL))
+    home_team2 = re.findall(make_regex(['<tr><td class="text-left"><span class="highlight"></span><a href(.*)</a>']), html_source(URL))
 
-    for i in r:
-        x.update(getEvents(i))
+    away_team1 = re.findall(make_regex(['<td class="text-left"><a href(.*)</a>']), html_source(URL))
+    away_team2 = re.findall(make_regex(['([^<tr>])<td class="text-left"><span class="highlight"></span><a href(.*)</a>']), html_source(URL))
 
-    date =  getNextPlayerEvent(x)
+    regex = r'>(.*)'
 
-    if date == 'No Next Event':
-       next_event_info = {'Date': 'Regular Season Over!', 'Weekday':'Had a Blast', 'Location': 'We\'ll do it again', 'Time': 'Offseason', 'Opponent': 'See You Next Season!', 'Start_Time': 'That Was Fun'}
-       return next_event_info
+    away_teams = [re.findall(regex, str(i))[0] for i in away_team1 + [j[1] for j in away_team2]]
+    home_teams = [re.findall(regex, str(i))[0] for i in home_team1 + home_team2]
 
+    x = [[a[0]] + [a[1]] + [1*(a[2] != my_team)*a[2] + 1*(a[3] != my_team)*a[3]] for a in zip(game_times, game_dates, home_teams, away_teams)]
 
-    return x[date]
-
-def getPlayerName(player='None'):
-    URL = 'https://chup-chombas.firebaseio.com/Players/{}.json'.format(player)
-    r = requests.get(URL).json()
-    return r['Name']
+    for i in x:
+        hockey_month = int(datetime.datetime.strptime('2018 ' + i[1].strip(), '%Y %a, %b %d').strftime("%m"))
+        hockey_day = int(datetime.datetime.strptime('2018 ' + i[1].strip(), '%Y %a, %b %d').strftime("%d"))
+        addEvent(start_time = i[0], location = location, opponent = i[2],year=2018, month=hockey_month, day=hockey_day, team=my_team)
 
 
 if __name__ == "__main__":
     #print(incrementTrip('Black'))
-    doAll()
+    my_team = 'Red'
+    location = 'Allen Hockey Rink'
+    year = 2018
+    URL = 'http://stats.pointstreak.com/players/print/players-team-schedule.html?teamid=709641&seasonid=17484'
+    add_allen_team(my_team=my_team, location='Allen Hockey Rink', URL=URL)
     #print(aggregate('JL'))
     #print type(datetime.date.today())
